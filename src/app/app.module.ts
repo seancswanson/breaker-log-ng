f708a22cebf7fc3a5fb6cf4a261aab6d88ac1e1a
Sean Swanson <seancswan@gmail.com>
2020-09-09 19:18:23 -0700

    Styled Nav and Footer, Added styled BG image to content-area.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 import { MovesComponent } from './moves/moves.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MovesComponent
+    MovesComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
